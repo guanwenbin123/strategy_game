fedc6cad09cd209f9bc87bbb8ac9649c69e33c75
guanwenbin123 <wenbin.guan@outlook.com>
2026-07-22 02:14:53 +0800

    Update v1.0.cpp

--- v1.0.cpp
+++ v1.0.cpp
@@ -13,8 +13,21 @@ class aa{
             int bb_S;
             int bb_G
         }
+        aa (){
+            bb dd;
+            dd.bb_M = ;
+            dd.bb_F = ;
+            dd.bb_W = ;
+            dd.bb_I = ;
+            dd.bb_S = ;
+            dd.bb_G = ;
+        }
         void cc(){
             cout << " " <<;
             
         }
+}
+int main(){
+    aa GAME.
+    GAME.cc;
 }
