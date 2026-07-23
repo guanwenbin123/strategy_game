60b80e6cd1f33ac940f144a81e32ad5a2b17454e
guanwenbin123 <wenbin.guan@outlook.com>
2026-07-23 20:05:35 +0800

    Update v1.0.cpp

--- v1.0.cpp
+++ v1.0.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class aa{
-    public:
-        int a;
-        string b;
-        struct bb{
-            int bb_M;
-            int bb_F;
-            int bb_W;
-            int bb_I;
-            int bb_S;
-            int bb_G
-        }
-        aa (){
-            bb dd;
-            dd.bb_M = ;
-            dd.bb_F = ;
-            dd.bb_W = ;
-            dd.bb_I = ;
-            dd.bb_S = ;
-            dd.bb_G = ;
-        }
-        void cc(){
-            cout << " " <<;
-            
-        }
+class aa {
+public:
+    int a;
+    string b;
+    struct bb {
+        int bb_M;
+        int bb_F;
+        int bb_W;
+        int bb_I;
+        int bb_S;
+        int bb_G;
+    };
+    aa() {
+        bb dd;
+        dd.bb_M = 500;//钱
+        dd.bb_F = 1000;//食物
+        dd.bb_W = 500;//木头
+        dd.bb_I = 200;//铁
+        dd.bb_Stone = 500;//石头
+		dd.bb_Steel = 0;//钢
+		dd.bb_silver = 0;//银
+
+    }
+    void cc() {
+        cout << " " << ;
+    }
 }
-int main(){
-    aa GAME.
-    GAME.cc;
+int main() {
+    aa GAME;
+    GAME.cc();
 }
